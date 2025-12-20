190ff1b7c39a4420747f93e559cd27211e3e6d84
Priyesh Kumar <priyeshkumar@Priyeshs-MacBook-Air.local>
2025-12-21 04:04:06 +0530

    add nat gateway

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -60,3 +60,44 @@ resource "aws_route_table_association"  "public" {
     subnet_id       = aws_subnet.public[count.index].id
     route_table_id  = aws_route_table.public.id
 }
+
+# Elastic IP for NAT
+resource "aws_eip" "nat" {
+    domain = "vpc"
+
+    tags = merge(var.tags, {
+        Name = "${var.tags["Environment"]}-nat-eip"
+    })
+}
+
+# NAT Gateway
+resource "aws_nat_gateway" "this" {
+    allocation_id = aws_eip.nat.id
+    subnet_id = aws_subnet.public[0].id
+
+    tags = merge(var.tags,  {
+        Name = "${var.tags["Environment"]}-nat"
+    })
+
+    depends_on = [ aws_internet_gateway.this ]
+}
+
+# Private route table
+resource "aws_route_table" "private" {
+    vpc_id = aws_vpc.this.id
+
+    route = {
+        cidr_block      = "0.0.0.0/0"
+        nat_gateway_id  = aws_nat_gateway.this.id
+    }
+
+    tags = merge(var.tags, {
+    Name = "${var.tags["Environment"]}-private-rt"
+  })
+}
+
+resource "aws_route_table_association" "private" {
+    count               = length(aws_subnet.private)
+    subnet_id           = aws_subnet.private[count.index].id
+    route_table_id      = aws_route_table.private.id
+}
--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -9,3 +9,8 @@ output "public_subnet_ids" {
 output "private_subnet_ids" {
     value   = aws_subnet.private[*].id
 }
+
+output "nat_eip_public_ip" {
+  description = "Public IP address of the NAT Gateway Elastic IP"
+  value       = aws_eip.nat.public_ip
+}
