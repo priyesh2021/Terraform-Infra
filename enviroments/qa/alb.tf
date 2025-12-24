78281812f3683f2d8df7b743991cf4f4f5e0e555
EC2 Default User <ec2-user@ip-172-31-14-204.ap-south-1.compute.internal>
2025-12-24 07:07:41 +0000

    bug fixes

--- enviroments/qa/alb.tf
+++ enviroments/qa/alb.tf
@@ -3,15 +3,15 @@
 module "qa_alb_sg" {
   source = "../../modules/sg"
 
-  name = "qa-alb-sg"
+  name   = "qa-alb-sg"
   vpc_id = module.vpc.vpc_id
 
   ingress_rule = [
     {
-        from_port   = 80
-        to_port     = 80
-        protocol    = "tcp"
-        cidr_block  = ["0.0.0.0/0"]
+      from_port  = 80
+      to_port    = 80
+      protocol   = "tcp"
+      cidr_block = ["0.0.0.0/0"]
     }
   ]
 
@@ -21,15 +21,15 @@ module "qa_alb_sg" {
 module "qa_alb" {
   source = "../../modules/alb"
 
-  name                  = "qa-alb"
-  vpc_id                = module.vpc.vpc_id
-  subnet_ids            = [module.vpc.public_subnet_ids]
-  security_group_ids    = [module.vpc.security_group_ids]
-  tags                  = var.tags
+  name               = "qa-alb"
+  vpc_id             = module.vpc.vpc_id
+  subnet_ids         = module.vpc.public_subnet_ids
+  security_group_ids = [module.qa_alb_sg.security_group_id]
+  tags               = var.tags
 }
 
 resource "aws_lb_target_group_attachment" "private_ec2" {
-  target_group_arn          = module.qa_alb.target_group_arn
-  target_id                 = module.qa_private_ec2.instance_id
-  port                      = 80
+  target_group_arn = module.qa_alb.target_group_arn
+  target_id        = module.qa_private_ec2.instance_id
+  port             = 80
 }
