b378ecc629a559677046d46dc31b96a00c954ef1
Priyesh Kumar <priyeshkumar@Priyeshs-MacBook-Air.local>
2026-01-05 17:49:40 +0530

    update lambda file

--- enviroments/qa/lambda.tf
+++ enviroments/qa/lambda.tf
@@ -1,16 +1,52 @@
+locals {
+  lambdas = {
+    user_service = {
+      function_name = "qa-user-lambda"
+      handler       = "app.handler"
+      runtime       = "python3.10"
+      zip_path      = "${path.module}/lambda_code/user/function.zip"
+      timeout       = 10
+      memory        = 128
+    }
+
+    order_service = {
+      function_name = "qa-order-lambda"
+      handler       = "app.handler"
+      runtime       = "python3.10"
+      zip_path      = "${path.module}/lambda_code/order/function.zip"
+      timeout       = 15
+      memory        = 256
+    }
+
+    payment_service = {
+      function_name = "qa-payment-lambda"
+      handler       = "app.handler"
+      runtime       = "python3.10"
+      zip_path      = "${path.module}/lambda_code/payment/function.zip"
+      timeout       = 30
+      memory        = 512
+    }
+  }
+}
+
 module "qa_lambda" {
+  for_each = local.lambdas
+
   source = "../../modules/lambda"
 
-  function_name = "qa-demo-lambda"
-  runtime       = "python3.10"
-  handler       = "app.handler"
-  timeout       = 15
-  lambda_zip_path = "${path.module}/lambda_code/function.zip"
+  function_name = each.value.function_name
+  runtime       = each.value.runtime
+  handler       = each.value.handler
 
+  lambda_zip_path = each.value.zip_path
   lambda_role_arn = "arn:aws:iam::590183742092:role/common-lambda-execution-role"
 
+  timeout      = each.value.timeout
+  memory_size = each.value.memory
+
   environment_variables = {
-    ENV = "qa"
+    ENV     = "qa"
+    SERVICE = each.key
   }
 
   tags = var.tags
--- enviroments/qa/lambda_code/payment/app.py
+++ enviroments/qa/lambda_code/payment/app.py
@@ -1,5 +1,5 @@
 def handler(event, context):
     return {
         "statusCode": 200,
-        "body": "Hello from ZIP-based Lambda"
+        "body": "Hello from payment team."
     }
